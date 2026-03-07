844d4d227b080a7ef45042a883682d23ced81c17
sudamsiths <sudamsiths@gmail.com>
2026-03-07 12:55:39 +0530

    feat: Implement an animated brand logo list component, integrate it into the home page, and add necessary global styling.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import ProductCategoryList from "@/components/ui/ProductCategoryList"
+import BrandLogoList from "@/components/common/BrandLogoList";
 
 
 export default function Home() {
     return (
         <main className="min-h-screen w-full">
+            <BrandLogoList />
             <ProductCategoryList />
         </main>
     );
