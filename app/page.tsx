5a5e9348607105213085e6c64f43c88146a37a3c
sudamsiths <sudamsiths@gmail.com>
2026-03-07 23:03:41 +0530

    feat: Add a new Footer component including navigation, social links, and newsletter subscription.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import ProductCategoryList from "@/components/ui/ProductCategoryList"
 import BrandLogoList from "@/components/common/BrandLogoList";
+import Footer from "@/components/common/Footer";
 
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
         <main className="min-h-screen w-full">
             <BrandLogoList />
             <ProductCategoryList />
+            <Footer />
         </main>
     );
 }
