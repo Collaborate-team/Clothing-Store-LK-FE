c414649c41f4739aea11d721672512463fa0fd63
sudamsiths <sudamsiths@gmail.com>
2026-03-10 21:19:11 +0530

    feat: Initialize homepage with global styling, a new contact form component, and basic layout.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import ProductCategoryList from "@/components/ui/ProductCategoryList"
 import BrandLogoList from "@/components/common/BrandLogoList";
 import Footer from "@/components/common/Footer";
+import ContactUsForm from "@/components/pages/ContactUsForm";
 
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
             <BrandLogoList />
             <ProductCategoryList />
             <Footer />
+            <ContactUsForm />
         </main>
     );
 }
